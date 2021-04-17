96986cae07d7ae8215424164b0167212265ed8c2
Aitoodeveloper <aidarous1143@gmail.com>
2021-04-17 07:32:04 +0300

    added file

--- index.js
+++ index.js
@@ -162,7 +162,21 @@ Using the vowelCounter function below do the following:
   HINT - try looking up the .includes() method
 */
 
+let magac= "aIdarOus mAhamEd "
 
-function vowelCounter(/*add your code here*/) {
-    /*add your code here*/
+function vowel_count(str1)
+{
+  var vowel_list = 'aeiouAEIOU';
+  var vcount = 0;
+  
+  for(var x = 0; x < str1.length ; x++)
+  {
+    if (vowel_list.indexOf(str1[x]) !== -1)
+    {
+      vcount += 1;
+    }
+  
+  }
+  return vcount;
 }
+console.log(vowel_count (magac) );
